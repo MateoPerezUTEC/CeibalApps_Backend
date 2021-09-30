c5010315af5a727c89c1b3f606aadef1223dcaed
MateoPerezUTEC <mateo.perez@estudiantes.utec.edu.uy>
2021-09-30 19:27:31 -0300

    Revert "Carga de archivos"
    This reverts commit 8973d5e2ed2f052ed988ee2a17ee3aeff577f6ad.

--- ceibalapps-servicios-backend/src/main/java/com/ceibal/ceibalApps/backend/CeibalappsServiciosBackendApplication.java
+++ ceibalapps-servicios-backend/src/main/java/com/ceibal/ceibalApps/backend/CeibalappsServiciosBackendApplication.java
@@ -1,26 +1,13 @@
 package com.ceibal.ceibalApps.backend;
 
-import javax.annotation.Resource;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.ceibal.ceibalApps.backend.models.services.FileStorageService;
-
 @SpringBootApplication
-public class CeibalappsServiciosBackendApplication implements CommandLineRunner{
-	 @Resource
-	  FileStorageService storageService;
+public class CeibalappsServiciosBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CeibalappsServiciosBackendApplication.class, args);
 	}
-
-	 @Override
-	  public void run(String... arg) throws Exception {
-	    storageService.deleteAll();
-	    storageService.init();
-	  }
 
 }
--- ceibalapps-servicios-backend/src/main/java/com/ceibal/ceibalApps/backend/models/dao/NoticiaDao.java
+++
@@ -1,9 +0,0 @@
-package com.ceibal.ceibalApps.backend.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ceibal.ceibalApps.backend.models.entities.Noticia;
-
-public interface NoticiaDao extends CrudRepository<Noticia, Long>{
-
-}
