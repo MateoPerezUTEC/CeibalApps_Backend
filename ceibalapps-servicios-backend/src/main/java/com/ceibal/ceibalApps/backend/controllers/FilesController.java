c31f897ed0cb056a89d7153a6f8d63e936975773
MateoPerezUTEC <mateo.perez@estudiantes.utec.edu.uy>
2021-10-15 10:42:09 -0300

    Imagenes y archivos
    imagenes por url
    archivos hasta 500mb

--- ceibalapps-servicios-backend/src/main/java/com/ceibal/ceibalApps/backend/controllers/FilesController.java
+++ ceibalapps-servicios-backend/src/main/java/com/ceibal/ceibalApps/backend/controllers/FilesController.java
@@ -1,18 +1,24 @@
 package com.ceibal.ceibalApps.backend.controllers;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -65,4 +71,17 @@ public class FilesController {
 				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
 				.body(file);
 	}
+	
+	@RequestMapping(value = "/api/images/{filename:.+}", method = RequestMethod.GET,
+            produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> getImage(@PathVariable String filename) throws IOException {
+
+        var imgFile = storageService.load(filename);
+        byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(bytes);
+    }
 }
